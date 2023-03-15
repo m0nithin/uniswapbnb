ed293a75dea692504dbe809aa4de57283b8fe750
Nithin <122009053+m0nithin@users.noreply.github.com>
2023-03-16 02:22:13 +0530

    Add files via upload

---
+++ _next/static/taj6kMgs7TfPFPGFsaWiD/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/968-a53bdc33ed77e9d9.js","static/chunks/pages/index-30fb42ce79327c79.js"],"/_error":["static/chunks/pages/_error-8e848193de3c139c.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
